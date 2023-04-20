b46d9e75d9b5e44f1db1499160da4f730b0b4421
Kevin Garcia <kevingarcore@gmail.com>
2023-04-20 03:01:18 -0600

    Actualización con el playgroun del sistema de hotel

--- pruebas.js
+++ pruebas.js
@@ -1,7 +1,11 @@
-const array = [1,3,2,4];
-let num = 0;
-const newarray = [];
+const dates = ['07/11','06/11','05/11'];
 
-// for (i = 0; i < array.length; i++) {
-//   if ()
-// }
+const ordenred = [...dates].sort((a, b) => {
+  const aDate = new Date(`${a} ${new Date().getFullYear()}`);
+  const bDate = new Date(`${b} ${new Date().getFullYear()}`);
+  console.log(aDate, bDate)
+  return aDate - bDate;
+
+});
+
+console.log(ordenred);
--- reservacion-hotel.js
+++ reservacion-hotel.js
@@ -17,3 +17,82 @@ checkIn: Fecha de llegada
 checkOut: Fecha de salida
 roomNumber: La habitación solicitada
 */
+
+function hotelSystem(rooms = 10) {
+  const reservations = [];
+
+  return {
+    searchReservation(id) {
+      if (!reservations.some(reservation => reservation.id === id)) {
+        throw new Error('La reservación no fue encontrada');
+      } else {
+        return reservations.find(reservation => reservation.id === id);
+      }
+    },
+
+    getSortReservations() {
+      const copy = [...reservations];
+      copy.sort((a, b) => {
+        const aDate = new Date(`${a.checkIn} ${new Date().getFullYear()}`);
+        const bDate = new Date(`${b.checkIn} ${new Date().getFullYear()}`);
+        return aDate - bDate;
+      });
+      return copy;
+    },
+
+    isAvailable(reservation) {
+      const resIn = reservation.checkIn;
+      const resOut = reservation.checkOut;
+      for (let room of reservations) {
+        let currentIn = room.checkIn;
+        let currentOut = room.checkOut;
+
+        if (resIn >= currentIn && resIn < currentOut ||
+            resOut > currentIn && resOut <= currentOut ||
+            resIn <= currentIn && resOut >= currentOut) {
+              if (reservation.roomNumber === room.roomNumber) {
+                return false;
+              }
+            }
+      }
+      return true;
+    },
+
+    addReservation(reservation) {
+      if (!this.isAvailable(reservation)) {
+        throw new Error('La habitación no está disponible');
+      }
+      reservations.push(reservation);
+      return `La reservación de ${reservation.name} quedo registrada`
+    },
+
+    removeReservation(id) {
+      const index = reservations.findIndex(item => item.id === id);
+      if (index === -1) {
+        throw new Error('La reservación que se busca remover no exite');
+      } else {
+        const removed = reservations[index];
+        reservations.splice(index, 1);
+        return removed;
+      }
+    },
+    
+    getReservations() {
+      return reservations;
+    },
+
+    getAvailableRooms(checkIn, checkOut) {
+      const availableRooms = [];
+      for (let i = 1; i <= rooms; i++) {
+        const posibleRes = {checkIn, checkOut, roomNumber: i};
+        if (this.isAvailable(posibleRes)) {
+          availableRooms.push(i);
+        }
+      }
+      return availableRooms;
+    }
+  }
+}
+
+const hotelElKevin = hotelSystem();
+console.log(hotelElKevin.searchReservation('A'));
